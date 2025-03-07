e9ea2a1f3bea937e047f5e98b1f168c1c570b0e8
jongwankim <jongwankim@jongwankimui-MacBookPro.local>
2025-03-07 21:58:43 +0900

    first commit

---
+++ apps/video/vite.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import federation from "@originjs/vite-plugin-federation"
+import { dependencies } from './package.json'
+// https://vite.dev/config/
+export default defineConfig({
+  build: {
+    target: "esnext"
+  },
+  plugins: [
+    react(),
+    federation({
+      name: 'video',
+      filename: 'remoteEntry.js',
+      // Modules to expose
+      exposes: {
+          './App': './src/App.tsx',
+      },
+      shared: {
+        "react":{
+          shareScope: 'shell',
+          requiredVersion: dependencies.react,
+        },
+        "react-dom":{
+          shareScope: 'shell',
+          requiredVersion: dependencies.react,
+        },
+      },
+  })
+  ],
+  server: {
+    port: 5173,
+  },
+  preview: {
+    port: 5173,
+  }
+  
+})
